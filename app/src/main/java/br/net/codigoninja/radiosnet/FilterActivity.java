40b8764cdc4c70caba4bcdad84dde03c40af7105
Augusto Lemes <gutonanet@hotmail.com>
2018-02-18 13:39:24 -0300

    alteracoes

---
+++ app/src/main/java/br/net/codigoninja/radiosnet/FilterActivity.java
@@ -0,0 +1,18 @@
+package br.net.codigoninja.radiosnet;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by guton on 13/02/2018.
+ */
+
+public class FilterActivity  extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.filter);
+    }
+
+}
